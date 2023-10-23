42a46f80533b536ac63b2e66094c1bae978182ca
dazariani <dato_davit@mail.ru>
2023-10-23 21:24:15 +0400

    Edit main.tsx

--- comments-app/src/main.tsx
+++ comments-app/src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
 import GlobalStyle from "./globalStyles";
-// import PlusIcon from "./assets/icon-plus.svg";
-// console.log(PlusIcon);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
